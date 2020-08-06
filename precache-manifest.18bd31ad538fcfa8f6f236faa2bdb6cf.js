571798158c254831a06d4995fb18c9492d69f26d
Jorge Ricardo Soares <jorgersoares@outlook.com>
2020-08-06 02:38:31 -0300

    Updates

---
+++ precache-manifest.18bd31ad538fcfa8f6f236faa2bdb6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05466f9c8220165decfc8d4ac9584eb7",
+    "url": "/covid-19-info/index.html"
+  },
+  {
+    "revision": "688b36eb8931dfa147b5",
+    "url": "/covid-19-info/static/css/main.a90f49e0.chunk.css"
+  },
+  {
+    "revision": "55892511ec403f0dd866",
+    "url": "/covid-19-info/static/js/2.f5aac021.chunk.js"
+  },
+  {
+    "revision": "c7a7e79b10ab645cab64be742e1dd9e5",
+    "url": "/covid-19-info/static/js/2.f5aac021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688b36eb8931dfa147b5",
+    "url": "/covid-19-info/static/js/main.1082511e.chunk.js"
+  },
+  {
+    "revision": "4935ea4de9ea75ed13af",
+    "url": "/covid-19-info/static/js/runtime-main.bf9c8972.js"
+  }
+]);
